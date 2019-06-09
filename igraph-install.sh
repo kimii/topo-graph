145e7729915c842b75558ef87f11ba850857f9ea
kimii <2545489745@qq.com>
2019-06-09 15:15:11 +0800

    Update igraph-install.sh

--- igraph-install.sh
+++ igraph-install.sh
@@ -2,7 +2,7 @@
 apt install libxml2
 apt install libxml2-dev
 ln -s /usr/include/libxml2/libxml/ /usr/include/
-wget https://igraph.org/nightly/get/c/igraph-0.7.1.tar.gz
+wget --no-check-certificate https://igraph.org/nightly/get/c/igraph-0.7.1.tar.gz
 tar zxvf igraph-0.7.1.tar.gz
 rm igraph-0.7.1.tar.gz
 cd igraph-0.7.1/
